4edcc083509d7a3da73338d8bcf654f088cd70db
Serhii Nazarenko <sergiobamper@gmail.com>
2023-02-22 19:21:54 +0200

    data fix, and text changes

--- discord_bot.py
+++ discord_bot.py
@@ -10,8 +10,6 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 USER_ID = int(os.getenv("USER_ID"))
 
-time = datetime.now().strftime("%d/%m/%Y %H:%M")
-
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
@@ -20,8 +18,8 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'We have logged in as {client.user}')
     await asyncio.gather(
-        compare_data(thread_en, "EN", 600),
-        compare_data(thread_ru, "RU", 600)
+        compare_data(thread_en, "EN", 20),
+        compare_data(thread_ru, "RU", 20)
     )
 
 
@@ -29,6 +27,7 @@ async def compare_data(thread, forum_lang, interval):
     prev_data = None
     while True:
         current_data = get_last_post(thread)
+        time = datetime.now().strftime("%d/%m/%Y %H:%M")
         if prev_data is not None and current_data == prev_data:
             print(f'{time} {forum_lang} forum data not changed')
         else:
@@ -45,10 +44,10 @@ async def compare_data(thread, forum_lang, interval):
 async def send_to_admin(current_data, thread, forum_lang):
     user = client.get_user(USER_ID)
     await user.send(f"A new user from the {forum_lang} forum want to join our guild.\n"
-                    f"Nickname: {current_data['username']}\n"
-                    f"Message: {current_data['message']}\n"
-                    f"Profile link: {current_data['profile_url']}\n"
-                    f"Forum link: {get_last_page_href(thread)}"
+                    f"**Forum link**: <{get_last_page_href(thread)}>\n"
+                    f"**Profile link**: <{current_data['profile_url']}>\n"
+                    f"**Nickname**: {current_data['username']}\n"
+                    f"**Message**: {current_data['message']}\n"
                     )
 
 
